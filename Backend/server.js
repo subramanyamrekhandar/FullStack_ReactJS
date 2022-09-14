d7a79d60edf08a177825a3d67b4e8ad5786dea72
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2022-09-14 11:05:47 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,69 @@
 
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql");
+//const mysql = require("mysql2");
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const db = require("./config/config");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
+
+
+
+app.post("/register", (req, res) => {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const password = req.body.password;
+  
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
+
+    db.query(
+      "INSERT INTO users (firstname,lastname,email,phone, password) VALUES (?,?,?,?,?)",
+      [firstname,lastname,email,phone,hash],
+      (err, result) => {
+        console.log(err);
+      }
+    );
+  });
+});
+
+app.listen(8080, () => {
+    console.log("running server 8080");
+  });
+  
