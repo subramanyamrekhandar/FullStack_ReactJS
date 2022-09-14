58f0143aaf7f4a1617351d81886fcaa29051029a
Subramanyam Rekhandar <53898299+subramanyamrekhandar@users.noreply.github.com>
2022-09-14 11:06:44 +0530

    Add files via upload

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,1 +1,17 @@
+const mysql = require("mysql2");
 
+const db = mysql.createConnection({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
+});
+
+
+db.connect((err)=>{
+    if(err) throw err;
+    console.log("DB connected");
+});
+
+
+module.exports = db;
